e3a06da4d0eb9fe014fedea5587e3e97c78e8c01
Abdul Awal <abdulawalarif0011@gmail.com>
2022-12-26 10:42:41 +0600

    home page Appbar is done

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overflow;
+  BigText(
+      {Key? key,
+      this.color = const Color(0xFF332d2b),
+      this.size = 20,
+      required this.text,
+      this.overflow = TextOverflow.ellipsis})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overflow,
+      maxLines: 1,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontSize: size,
+      ),
+    );
+  }
+}
